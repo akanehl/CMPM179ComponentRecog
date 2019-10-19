d8fa621db81c47e9592050a87c473cfdfb2f11e1
akanehl <akanehl@ucsc.edu>
2019-10-19 14:58:20 -0700

    Webcam suppott
    yay

---
+++ CMPM179ComponentRecogUnity/Assets/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    static WebCamTexture CameraFeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(CameraFeed == null)
+        {
+            CameraFeed = new WebCamTexture();
+        }
+
+        GetComponent<Renderer>().material.mainTexture = CameraFeed;
+
+        if(!CameraFeed.isPlaying)
+        {
+            CameraFeed.Play();
+        }
+    }
+
+}
