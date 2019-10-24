86f563af3088e730334f15bda0c32180cc4fc6b4
akanehl <akanehl@ucsc.edu>
2019-10-24 12:35:29 -0700

    Added ability to screenCapture. Press S to capture

--- CMPM179ComponentRecogUnity/Assets/CameraScript.cs
+++ CMPM179ComponentRecogUnity/Assets/CameraScript.cs
@@ -6,10 +6,16 @@ public class CameraScript : MonoBehaviour
 {
     static WebCamTexture CameraFeed;
 
+    public string filePath;
+    int fileNum;
+
 
     // Start is called before the first frame update
     void Start()
     {
+
+        fileNum = 0;
+
         if(CameraFeed == null)
         {
             CameraFeed = new WebCamTexture();
@@ -21,6 +27,24 @@ public class CameraScript : MonoBehaviour
         {
             CameraFeed.Play();
         }
+    }
+
+    void Update()
+    {
+      if( Input.GetKeyDown(KeyCode.S))
+        {
+            fileNum++;
+            string returnedVal = constFilePath();
+            ScreenCapture.CaptureScreenshot(returnedVal, 1);
+        }
+    }
+
+    string constFilePath()
+    {
+        string retVal;
+        retVal = filePath + "\\screencapture" + fileNum.ToString() + ".png";
+
+        return retVal;
     }
 
 }
